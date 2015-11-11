702e8f88ab091e30080f5e30bac96032ca9531bd
songzhiyuan <www.1319324030@qq.com>
2015-11-11 19:49:27 +0800

    第二次提交 排序
    增加define

--- main.cpp
+++ main.cpp
@@ -7,19 +7,20 @@
 //
 
 #include <iostream>
+#define N 5
 
 int main(int argc, const char * argv[])
 {
     int i,j;
     int t;
-    int a[10];
-    for(i=0;i<10;i++)
+    int a[N];
+    for(i=0;i<N;i++)
     {
         scanf("%d",&a[i]);
     }
-    for(i=0;i<10;i++)
+    for(i=0;i<N;i++)
     {
-        for(j=0;j<10-i-1;j++)
+        for(j=0;j<N-i-1;j++)
         {
             if(a[j]>a[j+1])
             {
@@ -29,7 +30,7 @@ int main(int argc, const char * argv[])
             }
         }
     }
-    for(i=0;i<10;i++)
+    for(i=0;i<N;i++)
     {
         printf("%d\n",a[i]);
     }
